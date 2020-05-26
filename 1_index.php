405677f79969a4be93cc2e29536c471b90bb09dc
spartacus1994 <me.cackel@gmail.com>
2020-05-26 17:34:32 +0545

    This is My first init

---
+++ 1_index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Java Script 1st Video Tutorial</title>
+<link rel="stylesheet" type="text/css" href="css/app.css">	
+</head>
+
+<body>
+
+<script src="js/app.js" type="text/javascript"></script>
+<script type="text/javascript">
+	
+	/* comment in java script */
+	document.write("<br><h1><strong><i>This is First Tutorial</i></strong></h1><br>"); 
+	document.write("<h1><strong>This is shubham Parajuli</strong></h1>");
+
+</script>
+</body>
+</html>
